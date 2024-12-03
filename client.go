4728e0d401de68d3368b1a9d74436e630fbfb1aa
Byron Williams <byron@fika.studio>
2024-12-03 16:55:23 +0100

    feat: add search cache

--- client.go
+++ client.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
 
 type SearchResponse struct {
@@ -24,13 +26,49 @@ type Cpus struct {
 	CPUInfoModelNames         []string `json:"cpuInfoModelNames"`
 }
 
-type Client struct{}
+type cacheItem struct {
+	expiresAt time.Time
+	item      *SearchResponse
+}
+
+type Client struct {
+	locker *sync.RWMutex
+	cache  map[string]cacheItem
+}
 
 func New() *Client {
-	return &Client{}
+	locker := &sync.RWMutex{}
+
+	cl := &Client{
+		cache:  map[string]cacheItem{},
+		locker: locker,
+	}
+
+	go func() {
+		for {
+			locker.Lock()
+			for k, v := range cl.cache {
+				if v.expiresAt.After(time.Now()) {
+					delete(cl.cache, k)
+				}
+			}
+			locker.Unlock()
+
+			time.Sleep(time.Second * 60)
+		}
+	}()
+
+	return cl
 }
 
 func (cl *Client) Search(ctx context.Context, name string) (*SearchResponse, error) {
+	cl.locker.RLock()
+	cachedVal, ok := cl.cache[name]
+	cl.locker.RUnlock()
+	if ok {
+		return cachedVal.item, nil
+	}
+
 	params := url.Values{}
 	params.Set("search", name)
 
@@ -50,6 +88,10 @@ func (cl *Client) Search(ctx context.Context, name string) (*SearchResponse, err
 	if err = json.NewDecoder(resp.Body).Decode(&out); err != nil {
 		return nil, err
 	}
+
+	cl.locker.Lock()
+	cl.cache[name] = cacheItem{expiresAt: time.Now().Add(time.Minute * 60), item: &out}
+	cl.locker.Unlock()
 
 	return &out, nil
 }
